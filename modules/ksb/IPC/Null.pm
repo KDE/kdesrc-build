0c1f4bcc57cf3b46228866fa22c66c2385b5edb0
Michael Pyne <mpyne@kde.org>
2012-11-24 20:51:57 -0500

    modularize: Move NullIPC to separate file (IPC::Null).

---
+++ modules/ksb/IPC/Null.pm
@@ -0,0 +1,40 @@
+package ksb::IPC::Null;
+
+# Dummy IPC module in case SysVIPC doesn't work or async mode is not needed.
+
+use strict;
+use warnings;
+use v5.10;
+
+use ksb::IPC;
+our @ISA = qw(ksb::IPC);
+
+sub new
+{
+    my $class = shift;
+    my $self = $class->SUPER::new;
+
+    $self->{'msgList'} = []; # List of messages.
+    return bless $self, $class; # OOP in Perl is so completely retarded
+}
+
+sub sendMessage
+{
+    my $self = shift;
+    my $msg = shift;
+
+    push @{$self->{'msgList'}}, $msg;
+    return 1;
+}
+
+sub receiveMessage
+{
+    my $self = shift;
+
+    return undef unless scalar @{$self->{'msgList'}} > 0;
+
+    return shift @{$self->{'msgList'}};
+}
+
+1;
+
