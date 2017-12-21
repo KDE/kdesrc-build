e1d38b76e5147e368b4c52d44d303dbd751eccfa
Michael Pyne <mpyne@kde.org>
2017-12-20 19:08:16 -0500

    GIT_SILENT Bump reported version.
    This is an appropriate time since I just committed a fairly significant
    chunk of work to remove use of the kde_projects.xml file.

--- modules/ksb/Version.pm
+++ modules/ksb/Version.pm
@@ -10,7 +10,7 @@ use IPC::Cmd qw(run can_run);
 
 # It is expected that future git tags will be in the form 'YY.MM' and will
 # be time-based instead of event-based as with previous releases.
-our $VERSION = '17.08';
+our $VERSION = '17.12';
 
 our $SCRIPT_PATH = ''; # For auto git-versioning
 
@@ -22,12 +22,12 @@ our @EXPORT = qw(scriptVersion);
 sub scriptVersion()
 {
     if ($SCRIPT_PATH && can_run('git') && -d "$SCRIPT_PATH/.git") {
-	my ($ok, $err_msg, undef, $stdout) = IPC::Cmd::run(
-	    command => ['git', "--git-dir=$SCRIPT_PATH/.git", 'describe'],
-	    verbose => 0);
-	my $output = $stdout->[0] // '';
-	chomp $output;
-	return "$SCRIPT_VERSION ($output)" if ($ok and $output);
+        my ($ok, $err_msg, undef, $stdout) = IPC::Cmd::run(
+            command => ['git', "--git-dir=$SCRIPT_PATH/.git", 'describe'],
+            verbose => 0);
+        my $output = $stdout->[0] // '';
+        chomp $output;
+        return "$SCRIPT_VERSION ($output)" if ($ok and $output);
     }
 
     return $SCRIPT_VERSION;
