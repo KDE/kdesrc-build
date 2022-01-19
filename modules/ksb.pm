3482e71cc3c79a3daf9aba2aa2803ceead840167
Michael Pyne <mpyne@kde.org>
2022-01-18 21:06:20 -0500

    boilerplate: Relax Perl version requirement and avoid new modules.
    We don't need the 'experimental' module (it's nice and it's core but we
    can easily achieve the effect we need to avoid adding dependencies).
    
    Plus (although this is fixed now) we're not using anything in Perl 5.28
    and are not likely to in the future, and some CI might still be using
    Perl 5.26 like ours was.
    
    See also !126 which is now unneeded.

--- modules/ksb.pm
+++ modules/ksb.pm
@@ -7,7 +7,7 @@ package ksb;
 #
 # Use by including 'use ksb;' at the beginning of each kdesrc-build source file.
 
-use v5.28;
+use v5.26;
 
 # These are imported to make *this* pragma strict and enable warnings. We will
 # then re-import these into our caller's namespace in our own sub import.
@@ -15,20 +15,29 @@ use strict;
 use warnings;
 
 # These are made available but not imported. They are only here so we can
-# re-import them into the caller's namespace.
-require experimental;
+# re-import them into the caller's namespace. We can rely on these to be
+# present even on minimal Perl.
+# NOTE: If we cannot rely on this, these need to be optional!
 require feature;
 
-my $REQUIRED_PERL_FEATURES = ':5.28';
-
-my @EXPERIMENTAL_FEATURES = qw(smartmatch signatures);
+my $REQUIRED_PERL_FEATURES = ':5.26';
+my @EXPERIMENTAL_FEATURES = qw(signatures);
 
 sub import
 {
     warnings->import;
     strict->import;
-    feature->import ($REQUIRED_PERL_FEATURES);
-    experimental->import (@EXPERIMENTAL_FEATURES);
+
+    # Enable features we use everywhere
+    feature->import($REQUIRED_PERL_FEATURES, @EXPERIMENTAL_FEATURES);
+
+    # Disable warnings for experimental features we're deliberately using.
+    warnings->unimport(map { "experimental::$_" } @EXPERIMENTAL_FEATURES);
+
+    # Manually disable experimental::smartmatch warnings which corresponds to a
+    # different named feature, 'switch', pulled in as part of the version
+    # feature flag.
+    warnings->unimport('experimental::smartmatch');
 }
 
 1;
