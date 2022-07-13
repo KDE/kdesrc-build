f12564c45af4c7edd73e41248d7243791db9f1f7
Michael Pyne <mpyne@kde.org>
2022-07-13 00:01:14 -0400

    autotools: Convert to use promises.
    This one is especially interesting because we have a series of
    log_command calls that we were able to turn into a chain of promises,
    including use of rejecting promises to allow early exit from that chain.
    
    Tested with nasm, which doesn't actually build due to other problems,
    but manages to run autogen.sh and then configure (it's getting hard to
    find autotools-using codes, luckily for us...).
    
    Once configureInternal is converted everywhere to return promises we'll
    be able to simplify handling of the update/build multiprocessing code.

--- modules/ksb/BuildSystem/Autotools.pm
+++ modules/ksb/BuildSystem/Autotools.pm
@@ -8,7 +8,8 @@ use parent qw(ksb::BuildSystem);
 
 use ksb::BuildException;
 use ksb::Debug;
-use ksb::Util;
+use ksb::Util qw(:DEFAULT run_logged_p);
+use Mojo::Promise;
 
 use List::Util qw(first);
 
@@ -17,10 +18,60 @@ sub name
     return 'autotools';
 }
 
-# Return value style: boolean
-sub configureInternal
+# Returns a promise that resolves to the specific configure command to use.
+#
+# This may execute commands to re-run autoconf to generate the script.
+#
+# If these commands fail the promise will reject.
+sub _findConfigureCommands ($self)
 {
-    my $self = assert_isa(shift, 'ksb::BuildSystem::Autotools');
+    my $module = $self->module();
+    my $sourcedir = $module->fullpath('source');
+
+    my $configureCommand = first { -e "$sourcedir/$_" } qw(configure autogen.sh);
+    my $configureInFile  = first { -e "$sourcedir/$_" } qw(configure.in configure.ac);
+
+    if ($configureCommand ne 'autogen.sh' && $configureInFile) {
+        return Mojo::Promise->resolve($configureCommand);
+    }
+
+    # If we have a configure.in or configure.ac but configureCommand is autogen.sh
+    # we assume that configure is created by autogen.sh as usual in some GNU Projects.
+    # So we run autogen.sh first to create the configure command and
+    # recheck for that.
+    if ($configureInFile && $configureCommand eq 'autogen.sh') {
+        p_chdir($sourcedir);
+        my $promise = run_logged_p($module, 'autogen', ["$sourcedir/$configureCommand"])
+            ->then(sub ($exitcode) {
+                die "Autogen failed with exit code $exitcode"
+                    if $exitcode != 0;
+            })->then(sub {
+                # Cleanup any stray Makefiles that may be present, if generated
+                return run_logged_p($module, 'distclean', [qw(make distclean)])
+                    if -e "$sourcedir/Makefile";
+                # nothing to do, return successful exit code
+                return 0;
+            })->then(sub ($exitcode) {
+                die "Failed to run make distclean, exit code $exitcode"
+                    if $exitcode != 0;
+            })->then(sub {
+                # Now recheck
+                $configureCommand = first { -e "$sourcedir/$_" } qw(configure autogen.sh);
+                return $configureCommand;
+            });
+
+        return $promise;
+    }
+
+    return Mojo::Promise->reject('No configure command available')
+        unless $configureCommand;
+
+    return Mojo::Promise->resolve($configureCommand);
+}
+
+# Return value style: boolean
+sub configureInternal ($self)
+{
     my $module = $self->module();
     my $sourcedir = $module->fullpath('source');
     my $installdir = $module->installationPath();
@@ -30,39 +81,22 @@ sub configureInternal
     my @bootstrapOptions = split_quoted_on_whitespace(
         $module->getOption('configure-flags', 'module') // '');
 
-    my $configureCommand = first { -e "$sourcedir/$_" } qw(configure autogen.sh);
+    my $result;
+    my $promise = $self->_findConfigureCommands()->then(sub ($configureCommand) {
+        p_chdir($module->fullpath('build'));
 
-    my $configureInFile = first { -e "$sourcedir/$_" } qw(configure.in configure.ac);
-
-    # If we have a configure.in or configure.ac but configureCommand is autogen.sh
-    # we assume that configure is created by autogen.sh as usual in some GNU Projects.
-    # So we run autogen.sh first to create the configure command and
-    # recheck for that.
-    if ($configureInFile && $configureCommand eq 'autogen.sh')
-    {
-        p_chdir($sourcedir);
-        my $err = log_command($module, 'autogen', ["$sourcedir/$configureCommand"]);
-        return 0 if $err != 0;
-
-        # We don't want a Makefile in the srcdir, so run make-distclean if that happened
-        # ... and hope that is enough to fix it
-        if (-e "$sourcedir/Makefile") {
-            $err = log_command($module, 'distclean', [qw(make distclean)]);
-            return 0 if $err != 0;
-        }
-
-        # Now recheck
-        $configureCommand = first { -e "$sourcedir/$_" } qw(configure autogen.sh);
-    }
-
-    croak_internal("No configure command available") unless $configureCommand;
-
-    p_chdir($module->fullpath('build'));
-
-    return log_command($module, 'configure', [
+        return run_logged_p($module, 'configure', [
             "$sourcedir/$configureCommand", "--prefix=$installdir",
             @bootstrapOptions
-        ]) == 0;
+        ]);
+    })->then(sub ($exitcode) {
+        $result = $exitcode;
+    })->catch(sub ($err) {
+        error ("\tError configuring $module: r[b[$err]");
+        return 0;
+    })->wait;
+
+    return $result == 0;
 }
 
 1;
