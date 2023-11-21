c998a918484943fecebc12ae0ac2230b2fdeeff4
Andrew Shark <ashark@linuxcomp.ru>
2023-11-21 14:59:05 +0300

    Extract packages lists from DATA

--- modules/ksb/FirstRun.pm
+++ modules/ksb/FirstRun.pm
@@ -89,6 +89,8 @@ RC
 
 =cut
 
+our $baseDir;
+
 sub yesNoPrompt {
     my $msg = shift;
 
@@ -100,7 +102,7 @@ sub yesNoPrompt {
 
 sub setupUserSystem
 {
-    my $baseDir = shift;
+    $baseDir = shift;
     my @setup_steps = @_;
     my $os = ksb::OSSupport->new;
     my $envShell = $ENV{'SHELL'} // 'undefined';
@@ -111,10 +113,10 @@ sub setupUserSystem
             _installSystemPackages($os);
         }
         if (grep { $_ eq "generate-config" } @setup_steps) {
-            _setupBaseConfiguration($baseDir);
+            _setupBaseConfiguration();
         }
         if (grep { $_ eq "update-shellrc" } @setup_steps) {
-            _setupShellRcFile($shellName, $baseDir);
+            _setupShellRcFile($shellName);
         }
     };
 
@@ -129,13 +131,16 @@ sub setupUserSystem
 
 # Internal functions
 
-# Reads from the __DATA__ section below and dumps the contents in a hash keyed
-# by filename (the @@ part between each resource).
+# Reads from the files from data/pkg and dumps the contents in a hash keyed by filename (the "[pkg/vendor/version]" part between each resource).
 my %packages;
 sub _readPackages
 {
+    my $vendor = shift;
+    my $version = shift;
+
     return \%packages if %packages;
 
+    open(my $file, '<', "$baseDir/data/pkg/$vendor.ini") or _throw("Cannot open file \"$baseDir/data/pkg/$vendor.ini\"");
     my $cur_file;
     my $cur_value;
     my $commit = sub {
@@ -144,11 +149,11 @@ sub _readPackages
         $cur_value = '';
     };
 
-    while(my $line = <DATA>) {
+    while(my $line = <$file>) {
         next if $line =~ /^\s*#/;
         chomp $line;
 
-        my ($fname) = ($line =~ /^@@ *([^ ]+)$/);
+        my ($fname) = ($line =~ /^\[ *([^ ]+) *\]$/);
         if ($fname) {
             $commit->();
             $cur_file = $fname;
@@ -157,6 +162,7 @@ sub _readPackages
             $cur_value .= "$line\n";
         }
     }
+    close($file);
 
     $commit->();
     return \%packages;
@@ -287,8 +293,6 @@ sub _getNumCoresForLowMemory($num_cores)
 
 sub _setupBaseConfiguration
 {
-    my $baseDir = shift;
-
     # According to XDG spec, if $XDG_CONFIG_HOME is not set, then we should
     # default to ~/.config
     my $xdgConfigHome = $ENV{XDG_CONFIG_HOME} // "$ENV{HOME}/.config";
@@ -342,7 +346,7 @@ DONE
 
 sub _setupShellRcFile
 {
-    my ($shellName, $baseDir) = @_;
+    my ($shellName) = @_;
     my $rcFilepath = undef;
     my $printableRcFilepath = undef;
     my $extendedShell = 1;
@@ -451,10 +455,7 @@ sub _findBestVendorPackageList
     my $os = shift;
 
     # Debian handles Ubuntu also
-    my @supportedDistros =
-        map  { s{^pkg/([^/]+)/.*$}{$1}; $_ }
-        grep { /^pkg\// }
-            keys %{_readPackages()};
+    my @supportedDistros = qw/alpine arch debian fedora freebsd gentoo mageia opensuse/;
 
     my $bestVendor = $os->bestDistroMatch(@supportedDistros);
     my $version = $os->vendorVersion();
@@ -465,7 +466,7 @@ sub _findBestVendorPackageList
 sub _packagesForVendor
 {
     my ($vendor, $version) = @_;
-    my $packagesRef = _readPackages();
+    my $packagesRef = _readPackages($vendor, $version);
 
     foreach my $opt ("pkg/$vendor/$version", "pkg/$vendor/unknown") {
         next unless exists $packagesRef->{$opt};
@@ -477,835 +478,3 @@ sub _packagesForVendor
 }
 
 1;
-
-__DATA__
-@@ pkg/debian/unknown
-# Perl support
-libyaml-libyaml-perl
-libio-socket-ssl-perl
-libjson-xs-perl
-liburi-perl
-# Basic build tools
-bison
-build-essential
-cmake
-flex
-gettext
-git
-gperf
-libssl-dev
-intltool
-meson
-ninja-build
-shared-mime-info
-clang-format
-apt-file
-# Qt-related
-qtwayland5-private-dev
-libdbusmenu-qt5-dev
-libqt5svg5-dev
-libqt5waylandclient5-dev
-libqt5x11extras5-dev
-qtbase5-private-dev
-qtdeclarative5-dev
-qtmultimedia5-dev
-qtquickcontrols2-5-dev
-qtscript5-dev
-qttools5-dev
-qtwayland5-dev-tools
-qtxmlpatterns5-dev-tools
-qt6-tools-dev
-qt6-declarative-dev
-qt6-wayland
-qt6-wayland-dev
-qt6-wayland-dev-tools
-qt6-base-private-dev
-qt6-speech-dev
-qt6-multimedia-dev
-libqt6svg6-dev
-libqt6core5compat6-dev
-libqt6opengl6-dev
-libqt6shadertools6-dev
-packagekit
-# Frameworks dependencies
-# .. polkit-qt-1
-libpolkit-gobject-1-dev
-libpolkit-agent-1-dev
-# .. kdoctools
-libxml2-dev
-libxml2-utils
-libxslt-dev
-docbook
-docbook-xsl
-docbook-xml
-# .. libksysguard
-libnl-3-dev
-libnl-route-3-dev
-libsensors-dev
-# .. kwindowsystem
-libwayland-dev
-libxcb-icccm4-dev
-libxcb-keysyms1-dev
-libxcb-res0-dev
-libxcb-xfixes0-dev
-libxcb-xkb-dev
-libxcb-xtest0-dev
-libxfixes-dev
-libxrender-dev
-wayland-protocols
-# .. kwallet
-libgcrypt-dev
-libgpgme11-dev
-libgpgmepp-dev
-# .. kactivities
-libboost-dev
-# .. kfilemetadata
-libattr1-dev
-# .. kidletime
-libxcb-sync-dev
-libx11-xcb-dev
-# .. kcalendarcore
-libical-dev
-# .. khtml
-libjpeg-dev
-libgif-dev
-# .. kjs
-libpcre3-dev
-# .. kglobalaccel
-libxcb-record0-dev
-# .. knotifications
-libcanberra-dev
-# .. karchive
-liblzma-dev
-libbz2-dev
-# .. plasma-workspace
-libqalculate-dev
-libxft-dev
-libxtst-dev
-libappstreamqt-dev
-libpackagekitqt5-dev
-libxcursor-dev
-# And others
-qt5keychain-dev
-libopenal-dev
-libopenjp2-7-dev
-qtlocation5-dev
-libraw-dev
-libsane-dev
-libsndfile1-dev
-libxcb-glx0-dev
-liblmdb-dev
-libsm-dev
-libqrencode-dev
-python3-setuptools
-# .. optional discover backends
-libjcat-dev
-libfwupd-dev
-libsnapd-qt-dev
-libflatpak-dev
-# kwin
-libgbm-dev
-libdrm-dev
-libxcvt-dev
-libxcb-randr0-dev
-libepoxy-dev
-libxcb-composite0-dev
-libxcb-shm0-dev
-libxcb-cursor-dev
-libxcb-damage0-dev
-libxcb-image0-dev
-libxcb-util-dev
-libxcb-present-dev
-libxcb-xinerama0-dev
-libxkbcommon-x11-dev
-# plasma
-libqalculate-dev
-libxcb-randr0-dev
-libxft-dev
-libxtst-dev
-# powerdevil
-libxcb-dpms0-dev
-# plasma-nm
-libnm-dev
-modemmanager-dev
-# plasma-pa
-libpulse-dev
-# plymouth-kcm
-libplymouth-dev
-# kscreenlocker
-libpam0g-dev
-# kpipewire
-libpipewire-0.3-dev
-libavcodec-dev
-libavformat-dev
-libswscale-dev
-# libkexiv2
-libexiv2-dev
-# kcalc
-graphviz
-doxygen
-xsltproc
-xmlto
-# poppler
-libnss3-dev
-libtiff-dev
-# gpgme
-texinfo
-# appstream
-libyaml-dev
-libsystemd-dev
-libgirepository1.0-dev
-libstemmer-dev
-itstool
-gi-docgen
-libzstd-dev
-
-@@ pkg/opensuse/unknown
-bison
-clang
-cmake
-cmake(packagekitqt5)
-cmake(packagekitqt6)
-cmake(QCoro6)
-cmake(Qt5Core)
-cmake(Qt5Multimedia)
-cmake(Qt5Qml)
-cmake(Qt5QuickControls2)
-cmake(Qt5QuickTemplates2)
-cmake(Qt5Script)
-cmake(Qt5Sensors)
-cmake(Qt5Svg)
-cmake(Qt5UiTools)
-cmake(Qt5WaylandClient)
-cmake(Qt5WebEngine)
-cmake(Qt5WebView)
-cmake(Qt5X11Extras)
-cmake(Qt5XmlPatterns)
-cmake(Qt6Concurrent)
-cmake(Qt6Core)
-cmake(Qt6Core5Compat)
-cmake(Qt6DBus)
-cmake(Qt6Gui)
-cmake(Qt6LinguistTools)
-cmake(Qt6Multimedia)
-cmake(Qt6Network)
-cmake(Qt6PrintSupport)
-cmake(Qt6Qml)
-cmake(Qt6Quick)
-cmake(Qt6QuickControls2)
-cmake(Qt6QuickTemplates2)
-cmake(Qt6QuickTest)
-cmake(Qt6QuickWidgets)
-cmake(Qt6Sensors)
-cmake(Qt6ShaderTools)
-cmake(Qt6Sql)
-cmake(Qt6Svg)
-cmake(Qt6Test)
-cmake(Qt6TextToSpeech)
-cmake(Qt6ToolsTools)
-cmake(Qt6UiTools)
-cmake(Qt6WaylandClient)
-cmake(Qt6WaylandCompositor)
-cmake(Qt6WebSockets)
-cmake(Qt6Widgets)
-docbook-xsl-stylesheets
-docbook_4
-doxygen
-flex
-libsndfile-devel
-gettext-runtime
-gettext-tools
-giflib-devel
-git
-gperf
-graphviz
-gtk-doc
-gtk3-devel
-intltool
-itstool
-libAppStreamQt-devel
-libboost_headers-devel
-libdbusmenu-qt5-devel
-libdisplay-info-devel
-libepub-devel
-libqt5-qtbase-common-devel
-libqt5-qtbase-private-headers-devel
-libqt5-qtimageformats-devel
-libqt5-qtwayland-private-headers-devel
-libQt5Core-private-headers-devel
-libQt5DesignerComponents5
-libsensors4-devel
-libxml2-tools
-lmdb-devel
-make
-meson
-ninja
-olm-devel
-openjpeg2-devel
-perl
-perl(IO::Socket::SSL)
-perl(JSON)
-perl(URI)
-perl(YAML::LibYAML)
-pkgconfig(accounts-qt5)
-pkgconfig(epoxy)
-pkgconfig(exiv2)
-pkgconfig(gbm)
-pkgconfig(gobject-introspection-1.0)
-pkgconfig(libattr)
-pkgconfig(libavcodec)
-pkgconfig(libavfilter)
-pkgconfig(libavformat)
-pkgconfig(libcanberra)
-pkgconfig(libcec)
-pkgconfig(libevdev)
-pkgconfig(libfakekey)
-pkgconfig(libical)
-pkgconfig(libnl-3.0)
-pkgconfig(libnm)
-pkgconfig(libopenssl)
-pkgconfig(libpcre)
-pkgconfig(libpipewire-0.3)
-pkgconfig(libpng)
-pkgconfig(libqalculate)
-pkgconfig(libqrencode)
-pkgconfig(libsignon-qt5)
-pkgconfig(libswscale)
-pkgconfig(libudev)
-pkgconfig(libva)
-pkgconfig(libxcvt)
-pkgconfig(libxml-2.0)
-pkgconfig(libxslt)
-pkgconfig(ModemManager)
-pkgconfig(nss)
-pkgconfig(pam)
-pkgconfig(sm)
-pkgconfig(wayland-protocols)
-pkgconfig(wayland-server)
-pkgconfig(xcb-cursor)
-pkgconfig(xcb-ewmh)
-pkgconfig(xcb-keysyms)
-pkgconfig(xcb-util)
-pkgconfig(xcursor)
-pkgconfig(xfixes)
-pkgconfig(xft)
-pkgconfig(xkbfile)
-pkgconfig(xorg-evdev)
-pkgconfig(xorg-libinput)
-pkgconfig(xorg-server)
-pkgconfig(xorg-synaptics)
-pkgconfig(xrender)
-pkgconfig(xtst)
-pkgconfig(xxf86vm)
-pkgconfig(yaml-0.1)
-plymouth-devel
-polkit-devel
-python3-setuptools
-qcoro-qt5-devel
-qt6-core-private-devel
-qt6-gui-private-devel
-qt6-printsupport-private-devel
-qt6-quick-private-devel
-qt6-waylandclient-private-devel
-shared-mime-info
-snowball-devel
-vlc-devel
-qt6-multimediawidgets-devel
-openal-soft-devel
-libxapian-devel
-cyrus-sasl-devel
-qt6-webenginewidgets-devel
-qt6-networkauth-devel
-zxing-cpp-devel
-libpoppler-qt6-devel
-liblcms2-devel
-qt6-statemachine-devel
-AppStream-devel
-xdotool
-
-@@ pkg/fedora/unknown
-appstream-qt-devel
-aha
-bison
-boost-devel
-bzip2
-cfitsio-devel
-chmlib-devel
-cmake
-cyrus-sasl-devel
-dbusmenu-qt5-devel
-djvulibre-devel
-docbook-style-xsl
-docbook-utils
-doxygen
-ebook-tools-devel
-eigen3-devel
-erfa-devel
-exiv2-devel
-flatpak-devel
-flex
-fuse3-devel
-fuse-devel
-gcc
-gcc-c++
-gettext
-gettext-devel
-giflib-devel
-git
-glew-devel
-gobject-introspection-devel
-gperf
-gpgmepp-devel
-gsl-devel
-gstreamer1-plugins-base-devel
-ibus-devel
-intltool
-itstool
-json-c-devel
-kcolorpicker-devel
-kdsoap-devel
-kf5-kdnssd-devel
-kf5-kplotting-devel
-kf5-libkdcraw-devel
-kimageannotator-devel
-libaccounts-qt5-devel
-libavcodec-free-devel
-libavfilter-free-devel
-libavformat-free-devel
-libavutil-free-devel
-libblack-hole-solver-devel
-libcap-devel
-libcanberra-devel
-libdisplay-info-devel
-libepoxy-devel
-libfakekey-devel
-libfreecell-solver-devel
-libgcrypt-devel
-libgit2-devel
-libical-devel
-libindi-devel
-libjpeg-turbo-devel
-libpcap-devel
-libqalculate-devel
-libmtp-devel
-libnl3-devel
-libnova-devel
-LibRaw-devel
-libsass-devel
-libsmbclient-devel
-libsndfile-devel
-libsodium-devel
-libspectre-devel
-libswscale-free-devel
-libssh-devel
-libtirpc-devel
-libuuid-devel
-libva-devel
-libwacom-devel
-libXcursor-devel
-libXft-devel
-libxcvt-devel
-libXext-devel
-libXtst-devel
-libXxf86vm-devel
-libxkbcommon-devel
-libxkbcommon-x11-devel
-libxkbfile-devel
-libxml2
-libzip-devel
-lm_sensors-devel
-make
-meson
-mpv-libs-devel
-openal-soft-devel
-openexr-devel
-openjpeg2-devel
-pam-devel
-pcre-devel
-perl(Digest::SHA)
-perl(FindBin)
-perl(IO::Compress::Gzip)
-perl(IO::Socket::SSL)
-perl(IPC::Cmd)
-perl(JSON::PP)
-perl(URI)
-perl(YAML::LibYAML)
-phonon-qt5-devel
-pipewire-devel
-pipewire-utils
-pkgconfig(dbus-1)
-pkgconfig(gbm)
-pkgconfig(gl)
-pkgconfig(gstreamer-1.0)
-pkgconfig(libassuan)
-pkgconfig(libattr)
-pkgconfig(libnm)
-pkgconfig(libpng)
-pkgconfig(libqrencode)
-pkgconfig(libxml-2.0)
-pkgconfig(libxslt)
-pkgconfig(lmdb)
-pkgconfig(ModemManager)
-pkgconfig(openssl)
-pkgconfig(polkit-gobject-1)
-pkgconfig(sm)
-pkgconfig(wayland-client)
-pkgconfig(wayland-protocols)
-pkgconfig(xapian-core)
-pkgconfig(xcb-cursor)
-pkgconfig(xcb-ewmh)
-pkgconfig(xcb-keysyms)
-pkgconfig(xcb-util)
-pkgconfig(xfixes)
-pkgconfig(xrender)
-python
-python3-chai
-python3-psutil
-python3-sphinx
-python3-setuptools
-qaccessibilityclient-devel
-qcoro-qt5-devel
-qgpgme-devel
-plymouth-devel
-qt5-*-devel
-qt5-qtbase-static
-qt5-qttools-static
-qt6-*-devel
-qtkeychain-qt5-devel
-PackageKit
-SDL2-devel
-sassc
-shared-mime-info
-signon-devel
-stellarsolver-devel
-systemd-devel
-texinfo
-wcslib-devel
-xdotool
-xmlto
-xorg-x11-drv-evdev-devel
-xorg-x11-drv-libinput-devel
-xorg-x11-drv-wacom-devel
-xkeyboard-config-devel
-# wayland
-expat-devel
-# poppler
-nss-devel
-libcurl-devel
-# appstream
-libyaml-devel
-libstemmer-devel
-gi-docgen
-
-@@ pkg/mageia/unknown
-bison
-boost
-cmake
-docbook-style-xsl
-docbook-utils
-flex
-gcc
-gcc-c++
-gettext
-gettext-devel
-giflib
-git
-gperf
-intltool
-lib64lmdb-devel
-libdbusmenu-qt5-devel
-make
-perl(IO::Socket::SSL)
-perl(IPC::Cmd)
-perl(JSON::PP)
-perl(URI)
-perl(YAML::LibYAML)
-pkgconfig(dbus-1)
-pkgconfig(gl)
-pkgconfig(gstreamer-1.0)
-pkgconfig(libattr)
-pkgconfig(libnm)
-pkgconfig(libpng)
-pkgconfig(libqrencode)
-pkgconfig(libxml-2.0)
-pkgconfig(libxslt)
-pkgconfig(ModemManager)
-pkgconfig(openssl)
-pkgconfig(polkit-gobject-1)
-pkgconfig(sm)
-pkgconfig(wayland-client)
-pkgconfig(xcb-keysyms)
-pkgconfig(xrender)
-python
-shared-mime-info
-
-@@ pkg/gentoo/unknown
-dev-lang/perl
-dev-libs/icu
-dev-libs/libdbusmenu-qt
-dev-perl/IO-Socket-SSL
-dev-perl/YAML-PP
-dev-perl/YAML-Syck
-dev-qt/designer:5
-dev-qt/linguist-tools:5
-dev-qt/linguist
-dev-qt/pixeltool
-dev-qt/qdoc:5
-dev-qt/qtconcurrent:5
-dev-qt/qtcore:5
-dev-qt/qtdbus:5
-dev-qt/qtdeclarative:5
-dev-qt/qtdiag:5
-dev-qt/qtgui:5
-dev-qt/qthelp:5
-dev-qt/qtmultimedia:5
-dev-qt/qtnetwork:5
-dev-qt/qtpaths:5
-dev-qt/qtprintsupport:5
-dev-qt/qtquickcontrols:5
-dev-qt/qtsql:5
-dev-qt/qttest:5
-dev-qt/qtwidgets:5
-dev-qt/qtx11extras:5
-dev-qt/qtxml:5
-dev-util/cmake
-dev-util/gperf
-dev-util/meson
-dev-util/ninja
-dev-vcs/git
-sys-devel/clang
-virtual/libintl
-
-@@ pkg/arch/unknown
-# Perl support
-perl-json
-perl-yaml-libyaml
-perl-io-socket-ssl
-perl-libwww
-perl-xml-parser
-perl-yaml-syck
-# Basic build tools
-# .. gnu
-autoconf
-automake
-bison
-flex
-gcc
-gperf
-make
-# .. llvm
-clang
-cmake
-# .. build systems
-ninja
-meson
-# .. rust
-corrosion
-# .. others
-boost
-docbook-xsl
-doxygen
-intltool
-pkg-config
-git
-gi-docgen
-# Qt-related
-qt6
-poppler-qt6
-qca-qt6
-qtkeychain-qt6
-qcoro-qt6
-qt5-base
-qt5-script
-qt5-websockets
-qt5-svg
-qt5-tools
-qt5-x11extras
-qca-qt5
-libaccounts-qt
-libdbusmenu-qt5
-poppler-qt5
-qtkeychain-qt5
-phonon-qt5
-packagekit
-# Others/Unsorted
-wayland-protocols
-qrencode
-signond
-xapian-core
-qgpgme
-kdsoap
-xsd
-xerces-c
-vlc
-ruby-sass
-eigen
-mlt
-freecell-solver
-sane
-vala
-check
-libolm
-xmlto
-itstool
-libdisplay-info
-python-sphinx
-python-setuptools
-enchant
-jasper
-openexr
-libutempter
-docbook-xsl
-shared-mime-info
-giflib
-libxss
-upower
-udisks2
-xorg-server-devel
-libpwquality
-libfakekey
-eigen
-xapian-core
-libdmtx
-ruby-test-unit
-plymouth
-#plasma-desktop
-xdotool
-# appstream
-gobject-introspection
-xf86-input-evdev
-python-chai
-
-@@ pkg/alpine/unknown
-alpine-sdk
-attr-dev
-autoconf
-automake
-bison
-boost-dev
-clang-extra-tools
-cmake
-corrosion
-curl-dev
-cyrus-sasl-dev
-doxygen
-eudev-dev
-exiv2-dev
-ffmpeg-dev
-flex
-giflib-dev
-gperf
-gpgme-dev
-grantlee-dev
-graphviz
-gst-plugins-base-dev
-gstreamer-dev
-kdsoap-dev
-lcms2-dev
-libaccounts-qt-dev
-libcanberra-dev
-libdbusmenu-qt-dev
-libdisplay-info-dev
-libdmtx-dev
-libepoxy-dev
-libgcrypt-dev
-libgit2-dev
-libical-dev
-libinput-dev
-libnl3-dev
-libqalculate-dev
-libqrencode-dev
-libsecret-dev
-libva-dev
-libxcvt-dev
-libxkbfile-dev
-libxmlb-dev
-libxrender-dev
-libxslt-dev
-libxtst-dev
-linux-pam-dev
-lm-sensors-dev
-lmdb-dev
-meson
-modemmanager-dev
-mpv-dev
-musl-fts-dev
-networkmanager-dev
-ninja
-olm-dev
-openjpeg-dev
-openldap-dev
-pcre-dev
-perl
-perl-io-socket-ssl
-perl-uri
-perl-yaml-libyaml
-pipewire-dev
-polkit-elogind-dev
-pulseaudio-dev
-py3-sphinx
-py3-setuptools
-qca-dev
-qcoro-dev
-qt5-qtbase-dev
-qt5-qtdeclarative-dev
-qt5-qtmultimedia-dev
-qt5-qtnetworkauth-dev
-qt5-qtquickcontrols2-dev
-qt5-qtscript-dev
-qt5-qtsensors-dev
-qt5-qtsvg-dev
-qt5-qttools-dev
-qt5-qtwayland-dev
-qt5-qtx11extras-dev
-qt5-qtxmlpatterns-dev
-qt6-qt5compat-dev
-qt6-qtbase-dev
-qt6-qtmultimedia-dev
-qt6-qtpositioning-dev
-qt6-qtsensors-dev
-qt6-qtspeech-dev
-qt6-qtsvg-dev
-qt6-qttools-dev
-qt6-qtwayland-dev
-qt6-qtwebengine-dev
-qt6-qtwebview-dev
-qtkeychain-dev
-signond-dev
-stb
-texinfo
-utfcpp
-wayland-protocols
-xapian-core-dev
-xcb-util-cursor-dev
-xcb-util-image-dev
-xcb-util-keysyms-dev
-xcb-util-wm-dev
-xkeyboard-config-dev
-xmlto
-yaml-dev
-
-@@ pkg/freebsd/unknown
-automake
-bison
-boost-all
-cmake
-docbook-xsl
-doxygen
-eigen
-gettext
-gmake
-gperf
-gpgme
-intltool
-libqrencode
-lmdb
-meson
-mlt7
-ninja
-p5-YAML-PP
-pkgconf
-qt5
-qt5-wayland
-wayland-protocols
-xorg
